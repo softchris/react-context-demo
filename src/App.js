74ce251874185a147d93c590b535d6992f40384c
softchris <noringc@gmail.com>
2019-03-05 13:42:02 +0000

    adding files to demo different uses of Context API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sample from './Sample';
+import AnyComponent from './AnyComponent';
+import CartProvider from './CartProvider';
 
 class App extends Component {
   render() {
@@ -8,17 +11,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Sample />
+          <AnyComponent />
+          <CartProvider />
         </header>
       </div>
     );
